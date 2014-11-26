f47596bb7be4f3dad5d00452f4708c83548ce89a
Matthias Holoch <mholoch@gmail.com>
2014-11-26 12:06:29 +0100

    implemented a region growing algorithm

--- assgn1/skinmodel.cpp
+++ assgn1/skinmodel.cpp
@@ -4,6 +4,66 @@
 
 using namespace std;
 
+class RegionGrower {
+  public:
+    RegionGrower(cv::Mat1d &prob_field)
+      : prob_field(prob_field) { }
+
+    /** Adds all points as seeds whose probability is bigger than the set threshold
+     * @returns how many seeds have been found.
+     */
+    int find_seeds(double seed_threshold) {
+      this->seed_threshold = seed_threshold;
+
+      for (int row = 0; row < prob_field.rows; ++row) {
+        for (int col = 0; col < prob_field.cols; ++col) {
+          if (prob_field(row, col) > seed_threshold) {
+            seeds.push_back(std::tuple<int, int>(row, col));
+          }
+        }
+      }
+
+      return seeds.size();
+    }
+
+    cv::Mat1d grow(double grow_threshold) {
+      if (seeds.size() <= 0) {
+        std::cout << "Warning: No seeds found. Forgot to call find_seeds?" << std::endl;
+      }
+
+      while (seeds.size() > 0) {
+        std::tuple<int, int> curr_pos = seeds.back();
+        seeds.pop_back();
+
+        for (int d_row = -1; d_row <= 1; d_row++) {
+          for (int d_col = -1; d_col <= 1; d_col++) {
+            int row = std::get<0>(curr_pos)+ d_row;
+            int col = std::get<1>(curr_pos)+ d_col; 
+            if (row >= 0 && col >= 0 && row < prob_field.rows && col < prob_field.cols) {
+              double new_prob = (prob_field(row, col) + prob_field(std::get<0>(curr_pos), std::get<1>(curr_pos))) / 2.0;
+              //std::cout << "avg prob is " << new_prob << ", threshold is " << grow_threshold;
+              // Only grow if the probabilites were different
+              if (new_prob != prob_field(row, col) && new_prob > grow_threshold) {
+                prob_field(row, col) = new_prob;
+                seeds.push_back(std::tuple<int, int>(row, col));
+                std::cout << " --> growing!" << std::endl;
+              } else {
+                //std::cout << " --> didn't grow." << std::endl;
+              }
+            }
+          }
+        }
+      }
+
+      return prob_field;
+    }
+
+  private:
+    cv::Mat1b prob_field;
+    std::vector<std::tuple<int, int> > seeds;
+    double seed_threshold;
+};
+
 /// Constructor
 SkinModel::SkinModel()
 {
